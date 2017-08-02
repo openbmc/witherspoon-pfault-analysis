ae5a772565d829b4455a9848be8a772e38eb35c6
Brandon Wyman <bjwyman@gmail.com>
2017-08-02 17:57:29 -0500

    WIP: Update to monitor VIN_UV_FAULT
    If the VIN_UV_FAULT bit turns on, create an error log.
    
    Change-Id: I9ba533ec3d24bae72d9198f7bd35f7da2fcd664c
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- elog-errors.hpp
+++ elog-errors.hpp
@@ -29,12 +29,79 @@ namespace Error
 } // namespace xyz
 } // namespace sdbusplus
 
+namespace sdbusplus
+{
+namespace xyz
+{
+namespace openbmc_project
+{
+namespace Power
+{
+namespace Fault
+{
+namespace Error
+{
+    struct PowerSupplyUnderVoltageFault;
+} // namespace Error
+} // namespace Fault
+} // namespace Power
+} // namespace openbmc_project
+} // namespace xyz
+} // namespace sdbusplus
+
 
 namespace phosphor
 {
 
 namespace logging
 {
+
+namespace xyz
+{
+namespace openbmc_project
+{
+namespace Power
+{
+namespace Fault
+{
+namespace _PowerSupplyUnderVoltageFault
+{
+
+struct STATUS_WORD
+{
+    static constexpr auto str = "STATUS_WORD=%d";
+    static constexpr auto str_short = "STATUS_WORD";
+    using type = std::tuple<std::decay_t<decltype(str)>,uint16_t>;
+    explicit constexpr STATUS_WORD(uint16_t a) : _entry(entry(str, a)) {};
+    type _entry;
+};
+
+}  // namespace _PowerSupplyUnderVoltageFault
+
+struct PowerSupplyUnderVoltageFault
+{
+    static constexpr auto L = level::ERR;
+    using STATUS_WORD = _PowerSupplyUnderVoltageFault::STATUS_WORD;
+    using metadata_types = std::tuple<STATUS_WORD>;
+
+};
+
+} // namespace Fault
+} // namespace Power
+} // namespace openbmc_project
+} // namespace xyz
+
+
+namespace details
+{
+
+template <>
+struct map_exception_type<sdbusplus::xyz::openbmc_project::Power::Fault::Error::PowerSupplyUnderVoltageFault>
+{
+    using type = xyz::openbmc_project::Power::Fault::PowerSupplyUnderVoltageFault;
+};
+
+}
 
 namespace xyz
 {
