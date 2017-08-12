b105f5482f2482c61c5eb411a9b3bed10ff545c2
Brandon Wyman <bjwyman@gmail.com>
2017-08-12 13:45:52 -0500

    Add in code to handle INPUT fault or warning
    If the INPUT fault or warning bit in the STATUS_WORD turns on, report a
    fault that includes STATUS_WORD and STATUS_INPUT values in the metadata.
    
    Change-Id: I2c0bc187357088a667dba1248be9a1c52f9dc073
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- power-supply/power_supply.cpp
+++ power-supply/power_supply.cpp
@@ -31,6 +31,11 @@ using namespace sdbusplus::xyz::openbmc_project::Power::Fault::Error;
 using PowerSupplyUnderVoltageFaultStatusWord = xyz::openbmc_project::Power::
         Fault::PowerSupplyUnderVoltageFault::STATUS_WORD;
 
+using PowerSupplyInputFaultStatusWord = xyz::openbmc_project::Power::
+                                        Fault::PowerSupplyInputFault::STATUS_WORD;
+using PowerSupplyInputFaultStatusInput = xyz::openbmc_project::Power::
+        Fault::PowerSupplyInputFault::STATUS_INPUT;
+
 namespace witherspoon
 {
 namespace power
@@ -77,17 +82,17 @@ void PowerSupply::analyze()
             // If count reaches 3, we have fault. If count reaches 0, fault is
             // cleared.
 
-            //TODO: INPUT FAULT or WARNING bit to check from STATUS_WORD
-            // pmbus-core update to read high byte of STATUS_WORD?
+            auto curInputFault = pmbusIntf.readBit(INPUT_FAULT_WARN,
+                                                   Type::Hwmon);
 
-            if ((curUVFault != vinUVFault) || inputFault)
+            if (curUVFault != vinUVFault)
             {
-
                 if (curUVFault)
                 {
                     std::uint16_t status_word = 0;
                     pmbusIntf.read(STATUS_WORD, Type::Debug,
-                                   reinterpret_cast<std::uint8_t*>(&status_word),
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&status_word),
                                    sizeof(status_word));
 
                     report<PowerSupplyUnderVoltageFault>(
@@ -101,6 +106,37 @@ void PowerSupply::analyze()
                                            inventoryPath.c_str()));
                     vinUVFault = false;
                 }
+
+            }
+
+            if (curInputFault != inputFault)
+            {
+                if (curInputFault)
+                {
+                    std::uint16_t status_word = 0;
+                    std::uint8_t  status_input = 0;
+
+                    pmbusIntf.read(STATUS_WORD, Type::Debug,
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&status_word),
+                                   sizeof(status_word));
+
+                    pmbusIntf.read(STATUS_INPUT, Type::Debug,
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&status_input),
+                                   sizeof(status_input));
+
+                    report<PowerSupplyInputFault>(
+                        PowerSupplyInputFaultStatusWord(status_word),
+                        PowerSupplyInputFaultStatusInput(status_input));
+
+                    inputFault = true;
+                }
+                else
+                {
+                    inputFault = false;
+                }
+
             }
         }
     }
