dffc4304611f941eddc8ddb0cf8b981bc5e2ee06
Brandon Wyman <bjwyman@gmail.com>
2017-08-25 18:59:54 -0500

    Add in support for detecting an output overcurrent fault
    When analyzing the STATUS_WORD bits, check if the IOUT_OC_FAULT bit is
    on. That bit indicates that an output overcurrent fault condition has
    occurred. Call out the power supply reporting that.
    
    Change-Id: I18c7cd981ffc1b73ab52c90d32ba34195013e65e
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- power-supply/power_supply.cpp
+++ power-supply/power_supply.cpp
@@ -110,10 +110,10 @@ void PowerSupply::analyze()
                 vinUVFault = true;
 
                 using metadata = xyz::openbmc_project::Power::Fault::
-                    PowerSupplyUnderVoltageFault;
+                        PowerSupplyUnderVoltageFault;
 
                 report<PowerSupplyUnderVoltageFault>(
-                    metadata::STATUS_WORD(statusWord));
+                        metadata::STATUS_WORD(statusWord));
             }
             else
             {
@@ -136,11 +136,11 @@ void PowerSupply::analyze()
                                sizeof(statusInput));
 
                 using metadata = xyz::openbmc_project::Power::Fault::
-                    PowerSupplyInputFault;
+                        PowerSupplyInputFault;
 
                 report<PowerSupplyInputFault>(
-                    metadata::STATUS_WORD(statusWord),
-                    metadata::STATUS_INPUT(statusInput));
+                        metadata::STATUS_WORD(statusWord),
+                        metadata::STATUS_INPUT(statusInput));
             }
             else
             {
@@ -162,15 +162,15 @@ void PowerSupply::analyze()
 
             if (powerOn)
             {
+                std::uint8_t statusVout = 0;
+                std::uint8_t statusIout = 0;
+                std::uint8_t statusMFR  = 0;
+
                 // Check PG# and UNIT_IS_OFF
                 if (((statusWord & status_word::POWER_GOOD_NEGATED) ||
                     (statusWord & status_word::UNIT_IS_OFF)) &&
                     !powerOnFault)
                 {
-                    std::uint8_t  statusVout = 0;
-                    std::uint8_t  statusIout = 0;
-                    std::uint8_t  statusMFR  = 0;
-
                     pmbusIntf.read(STATUS_INPUT, Type::Debug,
                                    reinterpret_cast<std::uint8_t*>
                                    (&statusInput),
@@ -205,6 +205,46 @@ void PowerSupply::analyze()
                         );
 
                     powerOnFault = true;
+                }
+
+                // Check for an output overcurrent fault.
+                if ((statusWord & status_word::IOUT_OC_FAULT) &&
+                    !outputOCFault)
+                {
+                    pmbusIntf.read(STATUS_INPUT, Type::Debug,
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&statusInput),
+                                   sizeof(statusInput));
+
+                    pmbusIntf.read(STATUS_VOUT, Type::Debug,
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&statusVout),
+                                   sizeof(statusVout));
+
+                    pmbusIntf.read(STATUS_IOUT, Type::Debug,
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&statusIout),
+                                   sizeof(statusIout));
+
+                    pmbusIntf.read(STATUS_MFR, Type::Debug,
+                                   reinterpret_cast<std::uint8_t*>
+                                   (&statusMFR),
+                                   sizeof(statusMFR));
+
+                    using metadata = xyz::openbmc_project::Power::Fault::
+                            PowerSupplyOutputOvercurrent;
+
+                    report<PowerSupplyOutputOvercurrent>(
+                            metadata::STATUS_WORD(statusWord),
+                            metadata::STATUS_INPUT(statusInput),
+                            metadata::STATUS_VOUT(statusVout),
+                            metadata::STATUS_IOUT(statusIout),
+                            metadata::MFR_SPECIFIC(statusMFR),
+                            metadata::CALLOUT_INVENTORY_PATH(
+                                    inventoryPath.c_str())
+                            );
+
+                    outputOCFault = true;
                 }
 
             }
@@ -287,6 +327,7 @@ void PowerSupply::powerStateChanged(sdbusplus::message::message& msg)
             readFailLogged = false;
             vinUVFault = false;
             inputFault = false;
+            outputOCFault = false;
             powerOnTimer.start(powerOnInterval, Timer::TimerType::oneshot);
         }
         else
