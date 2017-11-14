b90c29966f2617a97351a98694ab9bfeb77b15f3
Brandon Wyman <bjwyman@gmail.com>
2017-11-14 14:54:54 -0600

    Implement de-glitch for input fault
    If three consecutive reads in a row show the INPUT_FAULT_WARN and/or
    VIN_UV_FAULT on, create and log an error.
    
    Resolves: openbmc/openbmc#2484
    
    Change-Id: I35dedfc6acbb68bd8004c4fd1cc3d6dc799a9a17
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- power-supply/power_supply.cpp
+++ power-supply/power_supply.cpp
@@ -257,30 +257,21 @@ void PowerSupply::checkInputFault(const uint16_t statusWord)
 
     std::uint8_t  statusInput = 0;
 
-    if (!inputFault && ((statusWord & status_word::INPUT_FAULT_WARN) ||
-        (statusWord & status_word::VIN_UV_FAULT)))
+    if ((inputFault < FAULT_COUNT) &&
+        ((statusWord & status_word::INPUT_FAULT_WARN) ||
+         (statusWord & status_word::VIN_UV_FAULT)))
     {
-        faultFound = true;
-        inputFault = true;
-
-        util::NamesValues nv;
-        nv.add("STATUS_WORD", statusWord);
-        captureCmd(nv, STATUS_INPUT, Type::Debug);
-
-        using metadata = org::open_power::Witherspoon::Fault::
-                PowerSupplyInputFault;
-
-        report<PowerSupplyInputFault>(
-                metadata::RAW_STATUS(nv.get().c_str()),
-                metadata::CALLOUT_INVENTORY_PATH(inventoryPath.c_str()));
+        log<level::INFO>("INPUT_FAULT_WARN and/or VIN_UV_FAULT on",
+                         entry("STATUS_WORD=0x%04X", statusWord));
+        inputFault++;
     }
     else
     {
-        if ((inputFault) &&
+        if ((inputFault > 0) &&
             !(statusWord & status_word::INPUT_FAULT_WARN) &&
             !(statusWord & status_word::VIN_UV_FAULT))
         {
-            inputFault = false;
+            inputFault = 0;
             faultFound = false;
 
             statusInput = pmbusIntf.read(STATUS_INPUT, Type::Debug);
@@ -301,6 +292,22 @@ void PowerSupply::checkInputFault(const uint16_t statusWord)
             }
         }
     }
+
+    if (!faultFound && (inputFault >= FAULT_COUNT))
+    {
+        util::NamesValues nv;
+        nv.add("STATUS_WORD", statusWord);
+        captureCmd(nv, STATUS_INPUT, Type::Debug);
+
+        using metadata = org::open_power::Witherspoon::Fault::
+                PowerSupplyInputFault;
+
+        report<PowerSupplyInputFault>(
+                metadata::RAW_STATUS(nv.get().c_str()),
+                metadata::CALLOUT_INVENTORY_PATH(inventoryPath.c_str()));
+        faultFound = true;
+    }
+
 }
 
 void PowerSupply::checkPGOrUnitOffFault(const uint16_t statusWord)
@@ -546,7 +553,7 @@ void PowerSupply::clearFaults()
 {
     readFail = 0;
     readFailLogged = false;
-    inputFault = false;
+    inputFault = 0;
     powerOnFault = 0;
     outputOCFault = 0;
     outputOVFault = 0;
