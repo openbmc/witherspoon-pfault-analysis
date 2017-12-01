e05a071ce3f5cebd4c853a2a81c2f375a78b503a
Brandon Wyman <bjwyman@gmail.com>
2017-12-01 17:22:08 -0600

    Fill in the function for resolving errors
    Change-Id: Ic24841bca0b9fff7d70cc6343862f96f451ec468
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- power-supply/power_supply.cpp
+++ power-supply/power_supply.cpp
@@ -540,9 +540,65 @@ void PowerSupply::clearFaults()
     return;
 }
 
-void PowerSupply::resolveError(const std::string& callout, const std::string& message)
+void PowerSupply::resolveError(const std::string& callout,
+                               const std::string& message)
 {
-    // to be filled in.
+    constexpr auto ASSOCIATION_IFACE = "org.openbmc.Association";
+    constexpr auto ENDPOINTS_PROPERTY = "endpoints";
+    constexpr auto LOGGING_IFACE = "xyz.openbmc_project.Logging.Entry";
+    constexpr auto MESSAGE_PROPERTY = "Message";
+    constexpr auto RESOLVED_PROPERTY = "Resolved";
+    using namespace phosphor::logging;
+    using EndpointList = std::vector<std::string>;
+
+    try
+    {
+        auto path = callout + "/fault";
+        // Get the service name from the mapper for the fault callout
+        auto service = util::getService(path,
+                                        ASSOCIATION_IFACE,
+                                        bus);
+
+        // Use getProperty utility function to get log entries (endpoints)
+        EndpointList logEntries;
+        util::getProperty(ASSOCIATION_IFACE, ENDPOINTS_PROPERTY, path, service,
+                          bus, logEntries);
+
+        // go through each log entry that matches this callout path
+        for (const auto& logEntry : logEntries)
+        {
+            std::string logEntryService = util::getService(logEntry,
+                                                           LOGGING_IFACE,
+                                                           bus);
+
+            if (logEntryService.empty())
+            {
+                continue;
+            }
+
+            // Check to see if this logEntry has a message that matches.
+            std::string logMessage;
+            util::getProperty(LOGGING_IFACE, MESSAGE_PROPERTY, logEntry,
+                              logEntryService, bus, logMessage);
+
+            if (message == logMessage)
+            {
+                // Log entry matches call out and message, set Resolved to true
+                bool resolved = true;
+                util::setProperty(LOGGING_IFACE, RESOLVED_PROPERTY, logEntry,
+                                  logEntryService, bus, resolved);
+            }
+
+        }
+
+    }
+    catch (std::exception& e)
+    {
+        log<level::INFO>("Failed to resolve error",
+                         entry("CALLOUT=%s", callout.c_str()),
+                         entry("MESSAGE=%s", message.c_str()));
+    }
+
     return;
 }
 
