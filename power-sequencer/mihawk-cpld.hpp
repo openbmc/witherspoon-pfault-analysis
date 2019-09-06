45bcacbf0c2c1996cfdd6a15054e9882e4bc13db
AndyYFWang <Andy_YF_Wang@wistron.com>
2019-09-06 18:30:56 +0800

    Add CPLD class for Mihawk platform
    If PGOOD signal is abnormal when chassis poweron,
    connect to the CPLD-register-table via I2C to confirm the cause of the error.
    
    First confirm whether the PSU is present,
    then confirm whether the power_on_error signal is 1 (1 means abnormal).
    If the signal is 1,
    read the error-code-register analysis reason.
    
    Tested:
    Use command "obmcutil chassiskill" to trigger PGOOD error action analysis
    during chassis power on.
    
    Signed-off-by: Andy YF Wang <Andy_YF_Wang@wistron.com>
    Change-Id: I5f9c0d508627324a6c784ded125c28f0437bf52d

---
+++ power-sequencer/mihawk-cpld.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "device.hpp"
+#include "pmbus.hpp"
+#include <algorithm>
+#include <filesystem>
+
+#include <sdbusplus/bus.hpp>
+
+namespace witherspoon
+{
+namespace power
+{
+
+/**
+ * @class CPLD
+ *
+ * This class implements fault analysis for the CPLD
+ * power sequencer device.
+ *
+ */
+class CPLD : public Device
+{
+  public:
+    CPLD() = delete;
+    ~CPLD() = default;
+    CPLD(const CPLD&) = delete;
+    CPLD& operator=(const CPLD&) = delete;
+    CPLD(CPLD&&) = default;
+    CPLD& operator=(CPLD&&) = default;
+
+    /**
+     * Constructor
+     *
+     * @param[in] instance - the device instance number
+     * @param[in] bus - D-Bus bus object
+     */
+    CPLD(size_t instance, sdbusplus::bus::bus& bus);
+
+    /**
+     * Analyzes the device for errors when the device is
+     * known to be in an error state.  A log will be created.
+     */
+    void onFailure() override;
+
+    /**
+     * Checks the device for errors and only creates a log
+     * if one is found.
+     */
+    void analyze() override;
+
+    /**
+     * Clears faults in the device
+     */
+    void clearFaults() override
+    {
+    }
+
+  private:
+    /**
+     * If checkPoweronFault() returns "true",
+     * use ReadFromCPLDPSUErrorCode() to read CPLD-error-code-register
+     * to analyze the fail reason.
+     */
+    int ReadFromCPLDPSUErrorCode(int bus, int Addr);
+
+    /**
+     * Checks for PoweronFault on the CPLD-power_on-error-interrupt-bit-register whether is transfered to "1".
+     *
+     * @return bool - true if power_on fail.
+     */
+    bool checkPoweronFault();
+
+    /**
+     * Checks for PSU0 & PSU1 whether are "prensent".
+     *
+     * @return bool - true if PSU0 & PSU1 aren't "present".
+     */
+    bool checkPSUdevice();
+
+    /**
+     * The D-Bus bus object
+     */
+    sdbusplus::bus::bus& bus;
+
+};
+
+} // namespace power
+} // namespace witherspoon
