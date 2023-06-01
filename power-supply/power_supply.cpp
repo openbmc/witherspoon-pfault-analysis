98473dbcff7fd397ab9d3b242470029733d345cd
Patrick Williams <patrick@stwcx.xyz>
2023-06-01 08:50:43 -0500

    fix GCC 13 issues
    The latest version of GCC raises two issues:
    
        - cstdint is not included automatically as often so we need to
          explicitly include it whenever using `uint*` class types.
    
        - Certain move operations are detected as unnecessary and subverting
          RVO.
    
    Fix both of these issues.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I4124c91c8e5cce3b85a6284d3cc8dfc2a8494a76

--- power-supply/power_supply.cpp
+++ power-supply/power_supply.cpp
@@ -811,8 +811,8 @@ void PowerSupply::updateHistory()
     auto changed = recordManager->add(data);
     if (changed)
     {
-        average->values(std::move(recordManager->getAverageRecords()));
-        maximum->values(std::move(recordManager->getMaximumRecords()));
+        average->values(recordManager->getAverageRecords());
+        maximum->values(recordManager->getMaximumRecords());
     }
 }
 
